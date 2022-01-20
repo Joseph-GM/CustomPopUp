b0a8427ac8b52fd005ff85b0071cf8a496ada6f3
Joseph-GM <jkim.english75@gmail.com>
2022-01-21 06:55:28 +0900

    first commit

---
+++ CustomPopUp/CustomPopUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPopUpViewController.swift
+//  CustomPopUp
+//
+//  Created by Joseph Kim on 2022/01/20.
+//
+
+import UIKit
+
+class CustomPopUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("CustomPopUpViewController - viewDidLoad() Called")
+    }
+    
+}
--- CustomPopUp/ViewController.swift
+++ CustomPopUp/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onCreatePopUpClicked(_ sender: UIButton) {
+        
+        print("ViewController - onCreatePopUpClieed() called")
+        let stotryboard = UIStoryboard.init(name: "PoPUp", bundle: nil)
+        let alertPopUpVC = stotryboard.instantiateViewController(withIdentifier: "AlertPopUpVC")
+        alertPopUpVC.modalPresentationStyle = .overCurrentContext
+        alertPopUpVC.modalTransitionStyle = .crossDissolve
+        
+        self.present(alertPopUpVC, animated: true, completion: nil)
+        
+    }
+    
 }
 
